14a8f0e3ec6a897c111f87543ad94d707def8f97
Abid Rahman <arahman1@ualberta.ca>
2018-01-19 21:16:06 -0700

    Updated Height History link with d3

---
+++ js/heighthist_bg.js
@@ -0,0 +1,84 @@
+// ----------------------------------------------------
+// Abid Rahman
+// JS file for the NBA Height History Link Background
+// Filename: heighthist_bg.js
+// ----------------------------------------------------
+
+//Setup SVG size vars
+var w = 650,
+    h = 100;
+
+var color = d3.scale.category10();
+
+//Initialize the svg
+var heighthistSVG = d3.select("#heighthist-SVG").append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .on("mouseover", fillBars)
+    .on("mouseout", unfillBars)
+
+d3.select("#heighthist-SVG").style("background", "#000");
+
+for (var i = 0; i < 12; i++) {
+
+    var bar_color = color(getRndInteger(0, 9));
+    heighthistSVG.append("rect")
+        .attr("class", "bar")
+        .attr("x", i*60 + 5)
+        .attr("y", -5)
+        .attr("width", 30)
+        .attr("height", getRndInteger(20, 100) )
+        .attr("fill-opacity", 0.6)
+        .style("fill",  bar_color);
+}
+
+var titles = heighthistSVG.append("g");
+
+titles.append("text")
+    .attr("class", "link-title-hhist")
+    .attr("x", "50%")
+    .attr("y", 30)
+    .attr("dy", ".71em")
+    .attr("text-anchor", "middle")
+    .style("fill", "#fff")
+    .text("Height History");
+
+titles.append("text")
+    .attr("class", "link-desc-hhist")
+    .attr("x", "50%")
+    .attr("y", 70)
+    .attr("dy", ".5em")
+    .attr("text-anchor", "middle")
+    .style("fill", "#fff")
+    .text("A Look Into the NBA's Height Trends Over The Past 70 Years");
+
+function fillBars() {
+    d3.select("#heighthist-SVG").style("background", "#BB771F");
+    d3.select(".link-title-hhist").style("fill", "#000");
+    d3.select(".link-desc-hhist").style("fill", "#000");
+    heighthistSVG.selectAll(".bar")
+        .attr("stroke", "#fff")
+        .attr("fill-opacity", 0);
+}
+
+function unfillBars() {
+    d3.select("#heighthist-SVG").style("background", "#000");
+    d3.select(".link-title-hhist").style("fill", "#fff");
+    d3.select(".link-desc-hhist").style("fill", "#fff");
+    heighthistSVG.selectAll(".bar")
+        .style("fill", function(d) { return color(getRndInteger(0, 9)); })
+        .attr("stroke", "none")
+        .attr("fill-opacity", 0.6);
+}
+
+animate_bars();
+d3.interval(animate_bars, 5000);
+
+function animate_bars() {
+    
+    heighthistSVG.selectAll(".bar").transition()
+        .duration(function() { return getRndInteger(1000, 5000); })
+        .delay(function() { return getRndInteger(0, 2000); })
+        .attr("height", function() { return getRndInteger(20, 100); })
+        .style("fill", function(d) { return color(getRndInteger(0, 9)); });
+}
--- js/nbahwplot_bg.js
+++ js/nbahwplot_bg.js
@@ -18,57 +18,65 @@ var nbahwbgSVG = d3.select("#nbahwbg-SVG").append("svg")
     .on("mouseout", unfillDots)
   .append("g");
 
+d3.select("#nbahwbg-SVG").style("background", "#000");
+
 for (var i = 0; i < 70; i++) {
     nbahwbgSVG.append("circle")
         .attr("class", "dot")
         .attr("r", 6)
-        .attr("cx", Math.floor((Math.random() * 640) + 10) - 5)
-        .attr("cy", Math.floor((Math.random() * 90) + 10) - 5)
-        .attr("fill", "#fff")
-        .attr("opacity", 0.7)
-        .attr("stroke", color(Math.floor((Math.random() * 10) + 1)));
+        .attr("cx", getRndInteger(10,640) )
+        .attr("cy", getRndInteger(5,95) )
+        .attr("fill", "#000")
+        .attr("opacity", 0.9)
+        .attr("stroke", color(getRndInteger(0, 9)));
 }
 
 nbahwbgSVG.append("text")
     .attr("class", "link-title-nbahw")
-    .attr("x", 10)
-    .attr("y", 10)
+    .attr("x", "50%")
+    .attr("y", 30)
     .attr("dy", ".71em")
+    .attr("text-anchor", "middle")
+    .style("fill", "#fff")
     .text("Height vs Weight");
 
 nbahwbgSVG.append("text")
     .attr("class", "link-desc-nbahw")
-    .attr("x", 10)
-    .attr("y", 50)
-    .attr("dy", ".71em")
+    .attr("x", "50%")
+    .attr("y", 70)
+    .attr("dy", ".5em")
+    .attr("text-anchor", "middle")
+    .style("fill", "#fff")
     .text("Interactive Plot Of All 4550 Players to Have Ever Played in the NBA");
 
 animate();
-
-d3.interval(animate, 10000);
+d3.interval(animate, 3000);
 
 function animate() {
     nbahwbgSVG.selectAll(".dot").transition()
-        .duration(function(_) { return Math.floor((Math.random() * 10000) + 5000); })
-        .attr("cx", function(_) { return Math.floor((Math.random() * 640) + 10) - 5; })
-        .attr("cy", function(_) { return Math.floor((Math.random() * 90) + 10) - 5; });
+        .duration(function(_) { return getRndInteger(1000, 3000); })
+        .attr("cx", function(_) { return getRndInteger(10,640); })
+        .attr("cy", function(_) { return getRndInteger(5,95); });
 }
 
 function fillDots() {
     d3.select("#nbahwbg-SVG").style("background", "#BB771F");
-    d3.select(".link-title-nbahw").style("fill", "#fff");
-    d3.select(".link-desc-nbahw").style("fill", "#fff");
+    d3.select(".link-title-nbahw").style("fill", "#000");
+    d3.select(".link-desc-nbahw").style("fill", "#000");
     nbahwbgSVG.selectAll(".dot")
         .attr("stroke", "#fff")
         .attr("fill", "#BB771F");
 }
 
 function unfillDots() {
-    d3.select("#nbahwbg-SVG").style("background", "#fff");
-    d3.select(".link-title-nbahw").style("fill", "#000");
-    d3.select(".link-desc-nbahw").style("fill", "#000");
+    d3.select("#nbahwbg-SVG").style("background", "#000");
+    d3.select(".link-title-nbahw").style("fill", "#fff");
+    d3.select(".link-desc-nbahw").style("fill", "#fff");
     nbahwbgSVG.selectAll(".dot")
-        .attr("fill", "#fff")
-        .attr("stroke", function(_) { return color(Math.floor((Math.random() * 10) + 1)); });
+        .attr("fill", "#000")
+        .attr("stroke", function(_) { return color(getRndInteger(0, 9)); });
 }
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
