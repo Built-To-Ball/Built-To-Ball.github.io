2fa8bb7defa7a9dfa2385f2e81682b82bb52dc24
Abid Rahman <arahman1@ualberta.ca>
2018-02-02 00:06:01 -0700

    Encapsulate JS code within jQuery doc ready method

--- js/heighthist_bg.js
+++ js/heighthist_bg.js
@@ -4,79 +4,85 @@
 // Filename: heighthist_bg.js
 // ----------------------------------------------------
 
-//Setup SVG size vars
-var w = "100%",
-    h = 200;
+$( document ).ready(function () {
+    //Setup SVG size vars
+    var w = "100%",
+        h = 200;
 
-var color = d3.scale.category10();
+    var color = d3.scale.category10();
 
-//Initialize the svg
-var heighthistSVG = d3.select("#heighthist-SVG").append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .on("mouseover", fillBars)
-    .on("mouseout", unfillBars);
+    //Initialize the svg
+    var heighthistSVG = d3.select("#heighthist-SVG").append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .on("mouseover", fillBars)
+        .on("mouseout", unfillBars);
 
-d3.select("#heighthist-SVG").style("background", "#fff");
-
-for (var i = 0; i < 25; i++) {
-
-    var bar_color = color(getRndInteger(0, 9));
-    heighthistSVG.append("rect")
-        .attr("class", "bar")
-        .attr("x", i*60 + 5)
-        .attr("y", -5)
-        .attr("width", 30)
-        .attr("height", getRndInteger(20, 120) )
-        .attr("fill-opacity", 0.6)
-        .style("fill", color(getRndInteger(0, 9)))
-        .attr("stroke", "#000");
-}
-
-var titles = heighthistSVG.append("g");
-
-titles.append("text")
-    .attr("class", "link-title-hhist")
-    .attr("x", "50%")
-    .attr("y", 130)
-    .attr("dy", ".71em")
-    .attr("text-anchor", "middle")
-    .style("fill", "#000")
-    .text("Height History");
-
-titles.append("text")
-    .attr("class", "link-desc-hhist")
-    .attr("x", "50%")
-    .attr("y", 170)
-    .attr("dy", ".5em")
-    .attr("text-anchor", "middle")
-    .style("fill", "#000")
-    .text("A Look Into the NBA's Height Trends Over The Past 70 Years");
-
-function unfillBars() {
     d3.select("#heighthist-SVG").style("background", "#fff");
-    d3.select(".link-title-hhist").style("fill", "#000");
-    d3.select(".link-desc-hhist").style("fill", "#000");
-    heighthistSVG.selectAll(".bar")
-        .attr("stroke", "#000");
-}
 
-function fillBars() {
-    d3.select("#heighthist-SVG").style("background", "#000");
-    d3.select(".link-title-hhist").style("fill", "#fff");
-    d3.select(".link-desc-hhist").style("fill", "#fff");
-    heighthistSVG.selectAll(".bar")
-        .attr("stroke", "#fff");
-}
+    for (var i = 0; i < 25; i++) {
 
-animate_bars();
-d3.interval(animate_bars, 2000);
+        var bar_color = color(getRndInteger(0, 9));
+        heighthistSVG.append("rect")
+            .attr("class", "bar")
+            .attr("x", i*60 + 5)
+            .attr("y", -5)
+            .attr("width", 30)
+            .attr("height", getRndInteger(20, 120) )
+            .attr("fill-opacity", 0.6)
+            .style("fill", color(getRndInteger(0, 9)))
+            .attr("stroke", "#000");
+    }
 
-function animate_bars() {
-    
-    heighthistSVG.selectAll(".bar").transition()
-        .duration(function() { return getRndInteger(1000, 3000); })
-        .delay(function() { return getRndInteger(0, 2000); })
-        .attr("height", function() { return getRndInteger(20, 120); })
-        .style("fill", function(d) { return color(getRndInteger(0, 9)); });
-}
+    var titles = heighthistSVG.append("g");
+
+    titles.append("text")
+        .attr("class", "link-title-hhist")
+        .attr("x", "50%")
+        .attr("y", 130)
+        .attr("dy", ".71em")
+        .attr("text-anchor", "middle")
+        .style("fill", "#000")
+        .text("Height History");
+
+    titles.append("text")
+        .attr("class", "link-desc-hhist")
+        .attr("x", "50%")
+        .attr("y", 170)
+        .attr("dy", ".5em")
+        .attr("text-anchor", "middle")
+        .style("fill", "#000")
+        .text("A Look Into the NBA's Height Trends Over The Past 70 Years");
+
+    function unfillBars() {
+        d3.select("#heighthist-SVG").style("background", "#fff");
+        d3.select(".link-title-hhist").style("fill", "#000");
+        d3.select(".link-desc-hhist").style("fill", "#000");
+        heighthistSVG.selectAll(".bar")
+            .attr("stroke", "#000");
+    }
+
+    function fillBars() {
+        d3.select("#heighthist-SVG").style("background", "#000");
+        d3.select(".link-title-hhist").style("fill", "#fff");
+        d3.select(".link-desc-hhist").style("fill", "#fff");
+        heighthistSVG.selectAll(".bar")
+            .attr("stroke", "#fff");
+    }
+
+    animate_bars();
+    d3.interval(animate_bars, 2000);
+
+    function animate_bars() {
+        
+        heighthistSVG.selectAll(".bar").transition()
+            .duration(function() { return getRndInteger(1000, 3000); })
+            .delay(function() { return getRndInteger(0, 2000); })
+            .attr("height", function() { return getRndInteger(20, 120); })
+            .style("fill", function(d) { return color(getRndInteger(0, 9)); });
+    }
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+});
--- js/nbahwplot_bg.js
+++ js/nbahwplot_bg.js
@@ -4,79 +4,81 @@
 // Filename: nbahwplot_bg.js
 // ----------------------------------------------------
 
-//Setup SVG size vars
-var width = "100%",
-    height = 200;
+$( document ).ready(function () {
+    //Setup SVG size vars
+    var width = "100%",
+        height = 200;
 
-var color = d3.scale.category10();
+    var color = d3.scale.category10();
 
-//Initialize the svg
-var nbahwbgSVG = d3.select("#nbahwbg-SVG").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .on("mouseover", fillDots)
-    .on("mouseout", unfillDots)
-  .append("g");
+    //Initialize the svg
+    var nbahwbgSVG = d3.select("#nbahwbg-SVG").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mouseover", fillDots)
+        .on("mouseout", unfillDots)
+    .append("g");
 
-d3.select("#nbahwbg-SVG").style("background", "#000");
-
-for (var i = 0; i < 100; i++) {
-    nbahwbgSVG.append("circle")
-        .attr("class", "dot")
-        .attr("r", 6)
-        .attr("cx", getRndInteger(10,990) )
-        .attr("cy", getRndInteger(5,195) )
-        .attr("fill", "#000")
-        .attr("opacity", 0.6)
-        .attr("stroke", color(getRndInteger(0, 9)));
-}
-
-nbahwbgSVG.append("text")
-    .attr("class", "link-title-nbahw")
-    .attr("x", "50%")
-    .attr("y", 80)
-    .attr("dy", ".71em")
-    .attr("text-anchor", "middle")
-    .style("fill", "#fff")
-    .text("Height vs Weight");
-
-nbahwbgSVG.append("text")
-    .attr("class", "link-desc-nbahw")
-    .attr("x", "50%")
-    .attr("y", 120)
-    .attr("dy", ".5em")
-    .attr("text-anchor", "middle")
-    .style("fill", "#fff")
-    .text("Interactive Plot Of All 4550 Players to Have Ever Played in the NBA");
-
-animate();
-d3.interval(animate, 3000);
-
-function animate() {
-    nbahwbgSVG.selectAll(".dot").transition()
-        .duration(function(_) { return getRndInteger(1000, 3000); })
-        .attr("cx", function(_) { return getRndInteger(10,990); })
-        .attr("cy", function(_) { return getRndInteger(5,195); });
-}
-
-function fillDots() {
-    d3.select("#nbahwbg-SVG").style("background", "#fff");
-    d3.select(".link-title-nbahw").style("fill", "#000");
-    d3.select(".link-desc-nbahw").style("fill", "#000");
-    nbahwbgSVG.selectAll(".dot")
-        .attr("stroke", "#777")
-        .attr("fill", function(_) { return color(getRndInteger(0, 9)); });
-}
-
-function unfillDots() {
     d3.select("#nbahwbg-SVG").style("background", "#000");
-    d3.select(".link-title-nbahw").style("fill", "#fff");
-    d3.select(".link-desc-nbahw").style("fill", "#fff");
-    nbahwbgSVG.selectAll(".dot")
-        .attr("fill", "#000")
-        .attr("stroke", function(_) { return color(getRndInteger(0, 9)); });
-}
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+    for (var i = 0; i < 100; i++) {
+        nbahwbgSVG.append("circle")
+            .attr("class", "dot")
+            .attr("r", 6)
+            .attr("cx", getRndInteger(10,990) )
+            .attr("cy", getRndInteger(5,195) )
+            .attr("fill", "#000")
+            .attr("opacity", 0.6)
+            .attr("stroke", color(getRndInteger(0, 9)));
+    }
+
+    nbahwbgSVG.append("text")
+        .attr("class", "link-title-nbahw")
+        .attr("x", "50%")
+        .attr("y", 80)
+        .attr("dy", ".71em")
+        .attr("text-anchor", "middle")
+        .style("fill", "#fff")
+        .text("Height vs Weight");
+
+    nbahwbgSVG.append("text")
+        .attr("class", "link-desc-nbahw")
+        .attr("x", "50%")
+        .attr("y", 120)
+        .attr("dy", ".5em")
+        .attr("text-anchor", "middle")
+        .style("fill", "#fff")
+        .text("Interactive Plot Of All 4550 Players to Have Ever Played in the NBA");
+
+    animate();
+    d3.interval(animate, 3000);
+
+    function animate() {
+        nbahwbgSVG.selectAll(".dot").transition()
+            .duration(function(_) { return getRndInteger(1000, 3000); })
+            .attr("cx", function(_) { return getRndInteger(10,990); })
+            .attr("cy", function(_) { return getRndInteger(5,195); });
+    }
+
+    function fillDots() {
+        d3.select("#nbahwbg-SVG").style("background", "#fff");
+        d3.select(".link-title-nbahw").style("fill", "#000");
+        d3.select(".link-desc-nbahw").style("fill", "#000");
+        nbahwbgSVG.selectAll(".dot")
+            .attr("stroke", "#777")
+            .attr("fill", function(_) { return color(getRndInteger(0, 9)); });
+    }
+
+    function unfillDots() {
+        d3.select("#nbahwbg-SVG").style("background", "#000");
+        d3.select(".link-title-nbahw").style("fill", "#fff");
+        d3.select(".link-desc-nbahw").style("fill", "#fff");
+        nbahwbgSVG.selectAll(".dot")
+            .attr("fill", "#000")
+            .attr("stroke", function(_) { return color(getRndInteger(0, 9)); });
+    }
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+});
