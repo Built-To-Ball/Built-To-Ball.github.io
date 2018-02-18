27f60a2bf581054541f46e538c683a07acbf0121
Abid Rahman <arahman1@ualberta.ca>
2018-02-17 23:30:24 -0700

    Prettified all the code

--- js/heighthist_bg.js
+++ js/heighthist_bg.js
@@ -4,87 +4,106 @@
 // Filename: heighthist_bg.js
 // ----------------------------------------------------
 
-$( document ).ready(function () {
-    //Setup SVG size vars
-    var w = "100%",
-        h = 200;
+$(document).ready(function() {
+  //Setup SVG size vars
+  var w = "100%",
+    h = 200;
 
-    var color = d3.scale.category10();
-    var bgColor = "rgb(14, 24, 29)";
-    var fgColor = "#fff";
+  var color = d3.scale.category10();
+  var bgColor = "rgb(14, 24, 29)";
+  var fgColor = "#fff";
 
-    //Initialize the svg
-    var heighthistSVG = d3.select("#heighthist-SVG").append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .on("mouseover", fillBars)
-        .on("mouseout", unfillBars);
+  //Initialize the svg
+  var heighthistSVG = d3
+    .select("#heighthist-SVG")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .on("mouseover", fillBars)
+    .on("mouseout", unfillBars);
 
-    d3.select("#heighthist-SVG").style("background", bgColor);
+  d3.select("#heighthist-SVG").style("background", bgColor);
 
-    for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
+    var bar_color = color(getRndInteger(0, 9));
+    heighthistSVG
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", i * 60 + 5)
+      .attr("y", -5)
+      .attr("width", 30)
+      .attr("height", getRndInteger(20, 120))
+      .attr("fill-opacity", 0.6)
+      .style("fill", color(getRndInteger(0, 9)))
+      .attr("stroke", "#fff");
+  }
 
-        var bar_color = color(getRndInteger(0, 9));
-        heighthistSVG.append("rect")
-            .attr("class", "bar")
-            .attr("x", i*60 + 5)
-            .attr("y", -5)
-            .attr("width", 30)
-            .attr("height", getRndInteger(20, 120) )
-            .attr("fill-opacity", 0.6)
-            .style("fill", color(getRndInteger(0, 9)))
-            .attr("stroke", "#fff");
-    }
+  var titles = heighthistSVG.append("g");
 
-    var titles = heighthistSVG.append("g");
+  titles
+    .append("text")
+    .attr("class", "link-title-hhist")
+    .attr("x", "50%")
+    .attr("y", 130)
+    .attr("dy", ".71em")
+    .attr("text-anchor", "middle")
+    .style("fill", fgColor)
+    .text("Height History");
 
-    titles.append("text")
-        .attr("class", "link-title-hhist")
-        .attr("x", "50%")
-        .attr("y", 130)
-        .attr("dy", ".71em")
-        .attr("text-anchor", "middle")
-        .style("fill", fgColor)
-        .text("Height History");
+  titles
+    .append("text")
+    .attr("class", "link-desc-hhist")
+    .attr("x", "50%")
+    .attr("y", 170)
+    .attr("dy", ".5em")
+    .attr("text-anchor", "middle")
+    .style("fill", fgColor)
+    .text("A Look Into the NBA's Height Trends Over The Past 70 Years");
 
-    titles.append("text")
-        .attr("class", "link-desc-hhist")
-        .attr("x", "50%")
-        .attr("y", 170)
-        .attr("dy", ".5em")
-        .attr("text-anchor", "middle")
-        .style("fill", fgColor)
-        .text("A Look Into the NBA's Height Trends Over The Past 70 Years");
+  function unfillBars() {
+    d3
+      .select("#heighthist-SVG")
+      .transition()
+      .duration(200)
+      .style("background", bgColor);
+    d3.select(".link-title-hhist").style("fill", fgColor);
+    d3.select(".link-desc-hhist").style("fill", fgColor);
+    heighthistSVG.selectAll(".bar").attr("stroke", fgColor);
+  }
 
-    function unfillBars() {
-        d3.select("#heighthist-SVG").transition().duration(200).style("background", bgColor);
-        d3.select(".link-title-hhist").style("fill", fgColor);
-        d3.select(".link-desc-hhist").style("fill", fgColor);
-        heighthistSVG.selectAll(".bar")
-            .attr("stroke", fgColor);
-    }
+  function fillBars() {
+    d3
+      .select("#heighthist-SVG")
+      .transition()
+      .duration(500)
+      .style("background", fgColor);
+    d3.select(".link-title-hhist").style("fill", bgColor);
+    d3.select(".link-desc-hhist").style("fill", bgColor);
+    heighthistSVG.selectAll(".bar").attr("stroke", bgColor);
+  }
 
-    function fillBars() {
-        d3.select("#heighthist-SVG").transition().duration(500).style("background", fgColor);
-        d3.select(".link-title-hhist").style("fill", bgColor);
-        d3.select(".link-desc-hhist").style("fill", bgColor);
-        heighthistSVG.selectAll(".bar")
-            .attr("stroke", bgColor);
-    }
+  animate_bars();
+  d3.interval(animate_bars, 2000);
 
-    animate_bars();
-    d3.interval(animate_bars, 2000);
+  function animate_bars() {
+    heighthistSVG
+      .selectAll(".bar")
+      .transition()
+      .duration(function() {
+        return getRndInteger(1000, 3000);
+      })
+      .delay(function() {
+        return getRndInteger(0, 2000);
+      })
+      .attr("height", function() {
+        return getRndInteger(20, 120);
+      })
+      .style("fill", function(d) {
+        return color(getRndInteger(0, 9));
+      });
+  }
 
-    function animate_bars() {
-        
-        heighthistSVG.selectAll(".bar").transition()
-            .duration(function() { return getRndInteger(1000, 3000); })
-            .delay(function() { return getRndInteger(0, 2000); })
-            .attr("height", function() { return getRndInteger(20, 120); })
-            .style("fill", function(d) { return color(getRndInteger(0, 9)); });
-    }
-
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 });
--- js/nbahwplot_bg.js
+++ js/nbahwplot_bg.js
@@ -4,83 +4,112 @@
 // Filename: nbahwplot_bg.js
 // ----------------------------------------------------
 
-$( document ).ready(function () {
-    //Setup SVG size vars
-    var width = "100%",
-        height = 200;
+$(document).ready(function() {
+  //Setup SVG size vars
+  var width = "100%",
+    height = 200;
 
-    var color = d3.scale.category10();
-    var bgColor = "rgb(14, 24, 29)";
-    var fgColor = "#fff";
+  var color = d3.scale.category10();
+  var bgColor = "rgb(14, 24, 29)";
+  var fgColor = "#fff";
 
-    //Initialize the svg
-    var nbahwbgSVG = d3.select("#nbahwbg-SVG").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .on("mouseover", fillDots)
-        .on("mouseout", unfillDots)
+  //Initialize the svg
+  var nbahwbgSVG = d3
+    .select("#nbahwbg-SVG")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .on("mouseover", fillDots)
+    .on("mouseout", unfillDots)
     .append("g");
 
-    d3.select("#nbahwbg-SVG").style("background", bgColor);
+  d3.select("#nbahwbg-SVG").style("background", bgColor);
 
-    for (var i = 0; i < 50; i++) {
-        nbahwbgSVG.append("circle")
-            .attr("class", "dot")
-            .attr("r", 15)
-            .attr("cx", getRndInteger(10,990) )
-            .attr("cy", getRndInteger(5,195) )
-            .attr("fill", bgColor)
-            .attr("opacity", 0.6)
-            .attr("stroke", color(getRndInteger(0, 9)));
-    }
+  for (var i = 0; i < 50; i++) {
+    nbahwbgSVG
+      .append("circle")
+      .attr("class", "dot")
+      .attr("r", 15)
+      .attr("cx", getRndInteger(10, 990))
+      .attr("cy", getRndInteger(5, 195))
+      .attr("fill", bgColor)
+      .attr("opacity", 0.6)
+      .attr("stroke", color(getRndInteger(0, 9)));
+  }
 
-    nbahwbgSVG.append("text")
-        .attr("class", "link-title-nbahw")
-        .attr("x", "50%")
-        .attr("y", 80)
-        .attr("dy", ".71em")
-        .attr("text-anchor", "middle")
-        .style("fill", fgColor)
-        .text("Height vs Weight");
+  nbahwbgSVG
+    .append("text")
+    .attr("class", "link-title-nbahw")
+    .attr("x", "50%")
+    .attr("y", 80)
+    .attr("dy", ".71em")
+    .attr("text-anchor", "middle")
+    .style("fill", fgColor)
+    .text("Height vs Weight");
 
-    nbahwbgSVG.append("text")
-        .attr("class", "link-desc-nbahw")
-        .attr("x", "50%")
-        .attr("y", 120)
-        .attr("dy", ".5em")
-        .attr("text-anchor", "middle")
-        .style("fill", fgColor)
-        .text("Interactive Plot Of All 4550 Players to Have Ever Played in the NBA");
+  nbahwbgSVG
+    .append("text")
+    .attr("class", "link-desc-nbahw")
+    .attr("x", "50%")
+    .attr("y", 120)
+    .attr("dy", ".5em")
+    .attr("text-anchor", "middle")
+    .style("fill", fgColor)
+    .text(
+      "Interactive Plot Of All 4550 Players to Have Ever Played in the NBA"
+    );
 
-    animate();
-    d3.interval(animate, 3000);
+  animate();
+  d3.interval(animate, 3000);
 
-    function animate() {
-        nbahwbgSVG.selectAll(".dot").transition()
-            .duration(function(_) { return getRndInteger(1000, 3000); })
-            .attr("cx", function(_) { return getRndInteger(10,990); })
-            .attr("cy", function(_) { return getRndInteger(5,195); });
-    }
+  function animate() {
+    nbahwbgSVG
+      .selectAll(".dot")
+      .transition()
+      .duration(function(_) {
+        return getRndInteger(1000, 3000);
+      })
+      .attr("cx", function(_) {
+        return getRndInteger(10, 990);
+      })
+      .attr("cy", function(_) {
+        return getRndInteger(5, 195);
+      });
+  }
 
-    function fillDots() {
-        d3.select("#nbahwbg-SVG").transition().duration(500).style("background", fgColor);
-        d3.select(".link-title-nbahw").style("fill", bgColor);
-        d3.select(".link-desc-nbahw").style("fill", bgColor);
-        nbahwbgSVG.selectAll(".dot")
-            .attr("stroke", "#777")
-            .attr("fill", function(_) { return color(getRndInteger(0, 9)); });
-    }
+  function fillDots() {
+    d3
+      .select("#nbahwbg-SVG")
+      .transition()
+      .duration(500)
+      .style("background", fgColor);
+    d3.select(".link-title-nbahw").style("fill", bgColor);
+    d3.select(".link-desc-nbahw").style("fill", bgColor);
+    nbahwbgSVG
+      .selectAll(".dot")
+      .attr("stroke", "#777")
+      .attr("fill", function(_) {
+        return color(getRndInteger(0, 9));
+      });
+  }
 
-    function unfillDots() {
-        d3.select("#nbahwbg-SVG").transition().duration(200).style("background", bgColor);
-        d3.select(".link-title-nbahw").style("fill", fgColor);
-        d3.select(".link-desc-nbahw").style("fill", fgColor);
-        nbahwbgSVG.selectAll(".dot")
-            .attr("fill", bgColor)
-            .attr("stroke", function(_) { return color(getRndInteger(0, 9)); });
-    }
+  function unfillDots() {
+    d3
+      .select("#nbahwbg-SVG")
+      .transition()
+      .duration(200)
+      .style("background", bgColor);
+    d3.select(".link-title-nbahw").style("fill", fgColor);
+    d3.select(".link-desc-nbahw").style("fill", fgColor);
+    nbahwbgSVG
+      .selectAll(".dot")
+      .attr("fill", bgColor)
+      .attr("stroke", function(_) {
+        return color(getRndInteger(0, 9));
+      });
+  }
 
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 });
